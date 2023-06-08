8e6454999c1a9836220e41a4a205854190c5b8b5
indanaazulfa <indana_zulfa@student.ub.ac.id>
2023-06-08 10:12:57 +0700

    Migration Content

---
+++ database/migrations/2023_06_08_025348_content.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('contens', function (Blueprint $table) {
+            $table->id();
+            $table->string('judul',50);
+            $table->string('nama', 50);
+            $table->text('isi');
+            $table->binary('galery');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('contens');
+    }
+};
