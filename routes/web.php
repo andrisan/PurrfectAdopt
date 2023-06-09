6c4b739a8b3efb70e2f1eaa275f4e87ac602461f
adinrama <adinrama@student.ub.ac.id>
2023-06-09 20:45:05 +0700

    Modifikasi fitur lupa password

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,9 @@
 <?php
 
-use App\Http\Controllers\ForgotPasswordController;
-use App\Http\Controllers\ResetPasswordController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\NewPasswordController;
+use App\Http\Controllers\Auth\PasswordResetLinkController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -24,11 +24,11 @@ Route::middleware(['guest'])->group(function(){
   Route::get('/register', [RegisteredUserController::class, 'create'])->name('register');
   Route::post('/register', [RegisteredUserController::class, 'store']);
 
-  Route::get('/forgot-password', [ForgotPasswordController::class, 'index'])->name('password.request');
-  Route::post('/forgot-password', [ForgotPasswordController::class, 'forgotPasswordHandler'])->name('password.email');
+  Route::get('/forgot-password', [PasswordResetLinkController::class, 'create'])->name('password.request');
+  Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])->name('password.email');
 
-  Route::get('/reset-password/{token}', [ResetPasswordController::class, 'index'])->name('password.reset');
-  Route::post('/reset-password', [ResetPasswordController::class, 'resetPasswordHandler'])->name('password.update');
+  Route::get('/reset-password/{token}', [NewPasswordController::class, 'create'])->name('password.reset');
+  Route::post('/reset-password', [NewPasswordController::class, 'store'])->name('password.update');
 });
 
 Route::get('/home', function(){
