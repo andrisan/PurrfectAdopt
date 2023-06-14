93fb7714764de9dcb70b544160b8bd6437a52b6e
kindiaisyah <kindiaisyah17@student.ub.ac.id>
2023-06-14 10:36:03 +0700

    first commit

--- database/migrations/2023_06_08_070757_create_contentt.php
+++ database/migrations/2023_06_08_070757_create_contentt.php
@@ -11,10 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('contents', function (Blueprint $table) {
+        Schema::create('contentts', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('airline');
             $table->timestamps();
         });
     }
@@ -24,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('contents');
+        Schema::dropIfExists('contentts');
     }
 };
