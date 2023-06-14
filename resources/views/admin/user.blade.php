4cf5dec70ca665680cca49238eb4521ae4fce434
salsabillahalfi <salsabillahalfi@student.ub.ac.id>
2023-06-14 14:46:21 +0700

    change user admin page

--- resources/views/admin/user.blade.php
+++ resources/views/admin/user.blade.php
@@ -5,13 +5,55 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Beranda Admin</title>
 
-    <!-- Styles -->
-    <link rel="stylesheet" href="../css/style.css">
-
     <style>
-      .bg {
-        background-color: #D9D9D9;
+
+      .text-white {
+        --tw-text-opacity: 1;
+        color: rgba(255, 255, 255, var(--tw-text-opacity));
       }
+
+      .text-center {
+        text-align: center;
+      }
+
+      .font-medium {
+        font-weight: 500;
+      }
+
+      .grid {
+        display: grid;
+      }
+
+      .p-5 {
+        padding: 1.25rem;
+      }
+
+      .p-10 {
+        padding: 2.5rem;
+      }
+
+      .px-10 {
+        padding-left: 2.5rem;
+        padding-right: 2.5rem;
+      }
+      .px-24 {
+        padding-left: 6rem;
+        padding-right: 6rem;
+      }
+
+      .py-3 {
+        padding-top: 0.75rem;
+        padding-bottom: 0.75rem;
+      }
+
+      .place-items-center {
+        place-items: center;
+      }
+
+      .yellow-bg {
+        background-color: #FFF7D4;
+      }
+
       .bg-light{
         background-color: #FFD95A;
       }
@@ -19,36 +61,45 @@
       .bg-primary {
         background-color: #C07F00;
       }
+
+      .roundedd {
+        border-radius: 1.5rem;
+      }
       
+      .shadow {
+        --tw-shadow: 0 3px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
+        box-shadow: var(--tw-ring-offset-shadow, 0 0 #0000), var(--tw-ring-shadow, 0 0 #0000), var(--tw-shadow);
+      }
+
     </style>
 </head>
-<body style="font-family: Poppins;" class="bg">
+<body style="font-family: Poppins;" class="yellow-bg">
 
     <!--Table-->
-    <div class="p-10 px-15 font-bold text-gray-600 "></div>
-    <div class="grid p-10 gap-1 place-items-center">
-            <div class="flex flex-auto items-center justify-between  p-1 bg-light rounded shadow-sm">
-              <table class="min-w-full divide-y divide-gray-200 table-auto text-center text-white">
-                <thead class="bg-primary">
+    <div class="p-10"></div>
+    <div class="grid p-10 place-items-center">
+            <div class="p-5 bg-light rounded shadow">
+              <table class="text-center text-white">
+                <thead class="bg-primary rounded shadow">
                   <tr>
                     <th scope="col"
-                      class="px-5 py-2 text-center text-xs font-medium uppercase tracking-wider">
+                      class="px-10 py-3 font-medium">
                       ID User
                     </th>
                     <th scope="col"
-                      class="px-24 py-3 text-center text-xs font-medium uppercase tracking-wider">
+                      class="px-24 py-3 font-medium">
                       Name
                     </th>
                     <th scope="col"
-                      class="px-24 py-3 text-center text-xs font-medium uppercase tracking-wider">
+                      class="px-24 py-3 font-medium">
                       Email
                     </th>
                     <th scope="col"
-                      class="px-20 py-3 text-center text-xs font-medium uppercase tracking-wider">
+                      class="px-24 py-3 font-medium">
                       Data Kucing
-                     </th>
+                    </th>
                     <th scope="col"
-                      class="px-5 py-3 text-center text-xs font-medium uppercase tracking-wider">
+                      class="px-24 py-3 font-medium">
                       Aksi
                     </th>
                   </tr>
@@ -66,7 +117,7 @@
                       
                     </td>
                     <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                      <button>
+                      <button class="btn-primary">
 
                       </button>
                     </td>
@@ -76,7 +127,6 @@
                   </tr>
 
                   <!-- More people... -->
-                  <!-- dogmfdkgfdg -->
                 </tbody>
               </table>
             </div>
@@ -84,6 +134,6 @@
         </div>
       </div>
     </div>
-
+    </body>
 </body>
 </html>
