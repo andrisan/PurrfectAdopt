e9c5aadddca2080998bcfbdcce9dbbf2cb6d4688
GavinoBarcaDiyuNugroho <103358416+GavinoBarcaDiyuNugroho@users.noreply.github.com>
2023-06-15 17:39:27 +0700

    Update footer.blade.php
    Membetulkan beberapa button padding

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -15,19 +15,19 @@
     <div class="space-y-2 text-sm">
         <p class="text-base font-bold tracking-wide text-white">Our Office</p>
         <div class="flex items-center">
-            <i class="fas fa-map-marker-alt mr-1"></i> 
+            <i class="fas fa-map-marker-alt mr-3"></i> 
             <a href="https://www.google.com/maps" target="_blank" rel="noopener noreferrer" aria-label="Our address" title="Our address" class="transition-colors duration-300 text-deep-purple-accent-400 hover:text-deep-purple-800">
                 Jl. Veteran, Ketawanggede, Lowokwaru, Malang City, East Java 65145, ID
             </a>
         </div>
         <div class="flex items-center">
-            <i class="fas fa-phone mr-1"></i>
+            <i class="fas fa-phone mr-3"></i>
             <a href="tel:812-3456-7890" aria-label="Our phone" title="Our phone" class="transition-colors duration-300 text-deep-purple-accent-400 hover:text-deep-purple-800">
                 +6281234567890
             </a>
         </div>
         <div class="flex items-center">
-            <i class="fas fa-envelope mr-1"></i>
+            <i class="fas fa-envelope mr-3"></i>
             <a href="mailto:purrfectadopt@gmaail.com" aria-label="Our email" title="Our email" class="transition-colors duration-300 text-deep-purple-accent-400 hover:text-deep-purple-800">
                 purrfectadopt@gmail.com
             </a>
@@ -35,13 +35,13 @@
     </div>
     <div>
         <span class="text-base font-bold tracking-wide text-white">Social Media</span>
-        <div class="flex items-center mt-1 space-x-3">
-            <a class="flex items-center mr-6 text-white no-underline hover:text-gray-600" target="_blank" rel="noopener noreferrer" href="#">
+        <div class="flex items-center mt-1 space-x-3 p-">
+            <a class="flex items-center mr-1 text-white no-underline hover:text-gray-600" target="_blank" rel="noopener noreferrer" href="#">
                 <svg viewBox="0 0 24 24" class="w-5 h-5 fill-current">
                     <path d="M23.998 12c0-6.628-5.372-12-11.999-12C5.372 0 0 5.372 0 12c0 5.988 4.388 10.952 10.124 11.852v-8.384H7.078v-3.469h3.046V9.356c0-3.008 1.792-4.669 4.532-4.669 1.313 0 2.686.234 2.686.234v2.953H15.83c-1.49 0-1.955.925-1.955 1.874V12h3.328l-.532 3.469h-2.796v8.384c5.736-.9 10.124-5.864 10.124-11.853z"/>
                 </svg>
             </a>
-            <a class="flex items-center mr-6 text-white no-underline hover:text-gray-600" target="_blank" rel="noopener noreferrer" href="#">
+            <a class="flex items-center mr-1 text-white no-underline hover:text-gray-600" target="_blank" rel="noopener noreferrer" href="#">
                 <svg viewBox="0 0 24 24" class="w-5 h-5 fill-current">
                     <path d="M23.954 4.569a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.691 8.094 4.066 6.13 1.64 3.161a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.228-.616v.061a4.923 4.923 0 003.946 4.827 4.996 4.996 0 01-2.212.085 4.937 4.937 0 004.604 3.417 9.868 9.868 0 01-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 007.557 2.209c9.054 0 13.999-7.496 13.999-13.986 0-.209 0-.42-.015-.63a9.936 9.936 0 002.46-2.548l-.047-.02z"/>
                 </svg>
