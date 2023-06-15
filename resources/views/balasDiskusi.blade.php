ed078abd0132ce2fe00895be12e06c1aedf839ae
dyahyulianaa <dyahyulianaa@student.ub.ac.id>
2023-06-15 10:03:27 +0700

    balasDiskusi-Distributor
    update terbaru dari balasDiskusi-Distributor

--- resources/views/balasDiskusi.blade.php
+++ resources/views/balasDiskusi.blade.php
@@ -5,7 +5,7 @@
     <title>Diskusi</title>
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet" />
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet" />
-    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="balasDiskusi.css" />
   </head>
   <body>
     <div style="margin-left: 80px; margin-top: 20px" class="icon">
@@ -23,83 +23,35 @@
           </div>
           <h1 style="font-size: 20px; padding-left: 25px; padding-top: 25px; font-weight: bold">Ferry Pangateu</h1>
         </div>
-        <p style="font-size: 1.2vw; padding-left: 55px; padding-bottom: 15px">Bagaimana latar belakang kesehatan kucing ini? Apakah ada riwayat penyakit atau kondisi kesehatan tertentu yang perlu diperhatikan?</p>
-        <div class="flex">
-          <div style="padding-left: 70px">
-            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 14 14">
-              <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" d="m7 12.45l-5.52-5c-3-3 1.41-8.76 5.52-4.1c4.11-4.66 8.5 1.12 5.52 4.1Z" />
-            </svg>
-          </div>
-          <h1 style="font-size: 1.2vw; padding-left: 25px; padding-top: 5">1500 suka</h1>
-          <div style="padding-left: 70px">
-            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24">
-              <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 4H5a2 2 0 0 0-2 2v15l3.467-2.6a2 2 0 0 1 1.2-.4H19a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2z" />
-            </svg>
-          </div>
-          <h1 style="font-size: 1.2vw; padding-left: 25px; padding-top: 5">350 komentar</h1>
-        </div>
+        <p style="font-size: 1.2vw; padding-left: 55px; padding-bottom: 15px; padding-top: 20px">Bagaimana latar belakang kesehatan kucing ini? Apakah ada riwayat penyakit atau kondisi kesehatan tertentu yang perlu diperhatikan?</p>
       </div>
     </div>
 
     <div class="flex justify-center items-center">
-      <svg style="padding-left: 15px; padding-top: 30px" xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 24 24">
-        <path
-          fill="currentColor"
-          d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10s10-4.48 10-10S17.52 2 12 2zm0 4c1.93 0 3.5 1.57 3.5 3.5S13.93 13 12 13s-3.5-1.57-3.5-3.5S10.07 6 12 6zm0 14c-2.03 0-4.43-.82-6.14-2.88a9.947 9.947 0 0 1 12.28 0C16.43 19.18 14.03 20 12 20z"
-        />
-      </svg>
       <div class="kotak2">
-        <p style="font-size: 1.2vw; padding-left: 35px; padding-bottom: 15px; padding-top: 35px; padding-right: 60px">
-          Kucing ini telah menjalani pemeriksaan kesehatan secara menyeluruh oleh seorang dokter hewan terpercaya. Saat ini, kucing dalam kondisi sehat tanpa riwayat penyakit yang perlu diperhatikan. Kami memastikan bahwa kucing ini bebas
-          dari penyakit menular dan telah divaksinasi lengkap sesuai dengan jadwal yang disarankan oleh dokter hewan. Namun, penting untuk diingat bahwa sebagai calon pemilik, Anda perlu melakukan pemeriksaan kesehatan rutin dan memberikan
-          perawatan yang tepat untuk memastikan kucing tetap sehat dan bahagia.
+        <p style="font-size: 1.2vw; padding-left: 35px; padding-bottom: 15px; padding-top: 35px; padding-right: 60px" class="teks-tengah">
+          <b>"Keanggunan dan Keunikan Kucing British Shorthair: Ras Kucing yang Menggemaskan dan Menawan"</b>
+        </p>
+        <p style="font-size: 1.2vw; padding-left: 35px; padding-bottom: 15px; padding-top: 35px; padding-right: 60px" class="rata-kanan-kiri">
+          <spasi
+            >Kucing British Shorthair adalah salah satu ras kucing yang paling terkenal dan populer di dunia. Dikenal dengan wajahnya yang bulat dan postur tubuh yang kuat, mereka memiliki penampilan yang anggun dan daya tarik yang khas.
+            Artikel ini akan menjelajahi keunikan dan keistimewaan kucing British Shorthair, termasuk sejarah mereka, ciri-ciri fisik yang memukau, serta sifat dan kepribadian yang menarik.Ras kucing British Shorthair memiliki sejarah yang
+            panjang dan kaya. Awalnya, kucing-kucing ini adalah keturunan dari kucing domestik yang berasal dari Kepulauan Britania. Artikel ini akan melacak akar sejarah ras ini dan menjelaskan peran mereka dalam pengembangan ras kucing
+            modern. Salah satu ciri yang paling mencolok dari kucing British Shorthair adalah bentuk kepala mereka yang bulat dan pipi yang lebar. Bulu pendek dan tebal mereka memberikan tampilan yang mewah, sementara postur tubuh yang
+            kompak dan kokoh menambahkan kesan kekuatan. Dalam bagian ini, akan dijelaskan lebih lanjut tentang ciri-ciri fisik yang membedakan kucing British Shorthair dari ras kucing lainnya. Meskipun memiliki penampilan yang gagah,
+            kucing British Shorthair juga dikenal karena kepribadian mereka yang tenang, santai, dan ramah. Mereka cenderung menjadi kucing yang mudah bergaul dengan keluarga dan pemiliknya. Artikel ini akan menjelaskan sifat khas kucing
+            British Shorthair, termasuk kesetiaan, kemandirian, dan interaksi mereka dengan anak-anak dan hewan peliharaan lainnya. Merawat kucing British Shorthair adalah relatif mudah karena bulu mereka yang pendek. Namun, ada beberapa
+            hal penting yang perlu diperhatikan, seperti perawatan bulu, kebersihan gigi, dan pola makan yang sehat. Dalam bagian ini, akan diberikan tips dan saran mengenai perawatan kesehatan yang penting bagi kucing British Shorthair.
+            Kucing British Shorthair telah menjadi salah satu ras kucing yang paling populer di dunia, tidak hanya sebagai hewan peliharaan, tetapi juga sebagai bintang dalam iklan, film, dan media sosial. Artikel ini akan mengulas
+            popularitas mereka dan bagaimana kucing British Shorthair telah menjadi ikon dalam budaya populer.</spasi
+          >
+        </p>
+        <p style="font-size: 1.2vw; padding-left: 35px; padding-bottom: 15px; padding-top: 35px; padding-right: 60px" class="rata-kanan-kiri">
+          Kesimpulannya kucing British Shorthair adalah ras kucing yang mempesona dengan penampilan yang anggun dan kepribadian yang menarik. Sejarah mereka yang panjang, ciri-ciri fisik yang mencolok, serta sifat tenang dan ramah
+          menjadikan mereka pilihan yang populer sebagai hewan peliharaan. Dalam artikel ini, kita telah menjelajahi keunikan dan keistimewaan kucing British Shorthair. Apakah Anda sudah jatuh cinta pada pesona mereka atau ingin mengetahui
+          lebih banyak tentang ras ini, kucing British Shorthair selalu berhasil mencuri hati setiap orang yang mengenal mereka.
         </p>
         <div class="flex"></div>
-      </div>
-    </div>
-
-    <div class="flex justify-center items-center">
-      <svg style="padding-left: 15px; padding-top: 45px" xmlns="http://www.w3.org/2000/svg" width="115" height="115" viewBox="0 0 24 24">
-        <path
-          fill="currentColor"
-          d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10s10-4.48 10-10S17.52 2 12 2zm0 4c1.93 0 3.5 1.57 3.5 3.5S13.93 13 12 13s-3.5-1.57-3.5-3.5S10.07 6 12 6zm0 14c-2.03 0-4.43-.82-6.14-2.88a9.947 9.947 0 0 1 12.28 0C16.43 19.18 14.03 20 12 20z"
-        />
-      </svg>
-      <div class="kotak3">
-        <div style="padding-left: 25px; padding-top: 20px">
-          <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24">
-            <path
-              fill="currentColor"
-              d="M14.36 14.23a3.76 3.76 0 0 1-4.72 0a1 1 0 0 0-1.28 1.54a5.68 5.68 0 0 0 7.28 0a1 1 0 1 0-1.28-1.54ZM9 11a1 1 0 1 0-1-1a1 1 0 0 0 1 1Zm6-2a1 1 0 1 0 1 1a1 1 0 0 0-1-1Zm-3-7a10 10 0 1 0 10 10A10 10 0 0 0 12 2Zm0 18a8 8 0 1 1 8-8a8 8 0 0 1-8 8Z"
-            />
-          </svg>
-        </div>
-        <p style="font-size: 1.2vw; padding-left: 70px; padding-top: px; padding-right: 60px">Ketik pesan Anda disini</p>
-        <div style="padding-left: 825px">
-          <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24">
-            <path
-              fill="none"
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="1.5"
-              d="m21.438 11.662l-9.19 9.19a6.003 6.003 0 1 1-8.49-8.49l9.19-9.19a4.002 4.002 0 0 1 5.66 5.66l-9.2 9.19a2.001 2.001 0 1 1-2.83-2.83l8.49-8.48"
-            />
-          </svg>
-        </div>
-        <div style="padding-left: 890px">
-          <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24">
-            <g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5">
-              <path d="M2 19V9a2 2 0 0 1 2-2h.5a2 2 0 0 0 1.6-.8l2.22-2.96A.6.6 0 0 1 8.8 3h6.4a.6.6 0 0 1 .48.24L17.9 6.2a2 2 0 0 0 1.6.8h.5a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2Z" />
-              <path d="M12 17a4 4 0 1 0 0-8a4 4 0 0 0 0 8Z" />
-            </g>
-          </svg>
-        </div>
-      </div>
-      <div style="padding-top: 40px; padding-left: 25px; padding-right: 20px">
-        <svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" viewBox="0 0 24 24">
-          <path fill="currentColor" d="M12 2a10 10 0 0 1 10 10a10 10 0 0 1-10 10A10 10 0 0 1 2 12A10 10 0 0 1 12 2M8 7.71v3.34l7.14.95l-7.14.95v3.34L18 12L8 7.71Z" />
-        </svg>
       </div>
     </div>
   </body>
