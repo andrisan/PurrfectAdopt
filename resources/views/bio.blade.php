99ef8bfb31948557cbead67a635045c62894c797
MHilmiAdz <103296966+MHilmiAdz@users.noreply.github.com>
2023-06-15 12:05:29 +0700

    RESPONSIVE

--- resources/views/bio.blade.php
+++ resources/views/bio.blade.php
@@ -15,27 +15,41 @@
       @vite(['resources/css/app.css', 'resources/js/app.js'])
   </head>
   <!--kartu bio-->
-  <body class="bg-yellow-100">
-  <div class="flex flex-row justify-center mt-12 top-100">
-  <div class="w-64 p-6 bg-yellow-300 rounded shadow-md ml-6">
-  Nama saya {{ $bio->name }} dan saya berprofesi sebagai {{ $bio->profesi }}.
-  saya tinggal di {{ $bio->alamat }}. saya senang memelihara kucing   
-  </div>
-  
-  <div class="w-64 p-6 bg-yellow-300 rounded shadow-md ml-6">
-    <div class="flex items-center">
-      <img src="/images/IMAIL.png" alt="email">
-      <span style="padding-left:10px"></span>{{ $bio->email }}</div>
-      <br>
-      <div class="flex items-center">
-      <img src="/images/TELEPON 1.png" alt="telepon">
-      <span style="padding-left:10px"></span>{{ $bio->kontak }}</div>
-      <br>
-      <div class="flex items-center">
-      <img src="/images/IGE.png" alt="IGE">
-      <span style="padding-left:10px"></span>{{ $bio->profesi }}</div>
-  </div>
-</div>
+  <body class="bg-yellow-100 pl-20 pt-20">
+    <div class="pb-20">
+        <div class="absolute px-auto text-gray-700 text-5xl font-normal text-left font-poppins">
+        Bio and Contact Person
+        <img class="absolute right-[-25%] top-[-30%]" src="/images/ORANG.png" alt="ORANG">
+        </div>
+    </div>
+
+    <div class="justify-center flex flex-row mt-12 top-100"> <!--SEMUA KOTAK  -->
+      <div class="flex w-8/12 p-10 bg-yellow-300 rounded-2xl shadow-2xl  ml-0">
+        <div class="pl-6 pr-6 pb-6 pt-6 text-3xl">
+          Selain merawat kucing-kucing saya sendiri, saya juga aktif dalam
+          komunitas pecinta kucing. Saya bergabung dengan kelompok pecinta
+          kucing di daerah saya. Saya aktif dalam berbagi pengetahuan tentang
+          perawatan kucing melalui media sosial.
+        </div>
+      </div>
+      
+
+      <div class="pl-6 pr-24">
+        <div class="w-96 p-6 bg-yellow-300 rounded-2xl shadow-md ml-6">
+          <div class="flex items-center">
+            <img src="/images/IMAIL.png" alt="email">
+            <span style="padding-left:10px"></span>m.hilmi.adz@gmail.com</div>
+            <br>
+            <div class="flex items-center">
+            <img src="/images/TELEPON 1.png" alt="telepon">
+            <span style="padding-left:10px"></span>09182301928</div>
+            <br>
+            <div class="flex items-center">
+            <img src="/images/IGE.png" alt="IGE">
+            <span style="padding-left:10px"></span>Karyawan</div>
+        </div>
+      </div>
+    </div>
 
   </body>
 </html>
