74b7012288f46e5ef58e69eff628d05b1045706c
Alfiansyah Pradana <alfiansyah46@student.ub.ac.id>
2023-06-16 11:12:57 +0700

    ganti font

--- resources/views/admin/kucing.blade.php
+++ resources/views/admin/kucing.blade.php
@@ -4,7 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Purrfect Adopt</title>
+    
+    <!-- icon pack -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    
+    <link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
+
     <style>
+
 body{
   margin: 0;
   padding: 0;
