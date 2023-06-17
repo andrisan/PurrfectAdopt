e3421ce2dd87a51b5dad286298b0c49fc73f33f5
Taufik Hidayat <taufikhdyt@student.ub.ac.id>
2023-06-17 12:20:16 +0700

    menambahkan route di header

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -2,7 +2,7 @@
     <div class="w-full flex items-center justify-between">
         <div class="flex items-center">
             <a class="flex items-center text-white no-underline hover:no-underline font-bold text-2xl lg:text-4xl ml-4"
-                href="{{ route('welcome') }}">
+                href="#">
                 <x-application-logo class="block h-8 pr-2 w-auto fill-current" />
                 <span class="font-semibold text-lg md:text-xl">PurrfectAdopt</span>
             </a>
@@ -12,13 +12,13 @@
                 class="inline-block text-white no-underline text-center h-10 p-2 md:h-auto md:p-4 transform hover:scale-105 duration-300 ease-in-out">
                 Home
             </a>
-            <a href="#"
+            <a href="{{ route('article') }}"
                 class="inline-block text-white no-underline text-center h-10 p-2 md:h-auto md:p-4 transform hover:scale-105 duration-300 ease-in-out">
                 Article
             </a>
             <a href="#"
                 class="inline-block text-white no-underline text-center h-10 p-2 md:h-auto md:p-4 transform hover:scale-105 duration-300 ease-in-out">
-                My Chat
+                My Pet
             </a>
             <div class="relative ml-auto mr-5">
                 <a href="#"
