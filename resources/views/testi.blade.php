fbe24e96489632516f0e93722c892e5dbfff49b4
ameliaintan117 <ameliaintan.p117@gmail.com>
2023-06-18 15:39:22 +0700

    Update testimoni
    Menambahkan images pada testimoni agar dapat tampil pada halaman website

--- resources/views/testi.blade.php
+++ resources/views/testi.blade.php
@@ -90,6 +90,95 @@
         </div>
       </div>
     </div>
+
+    <div class="container mx-auto">
+    <div class="box mx-auto">
+      <div class="wrapper-header">
+        <img src="/images/bintang.png" >
+        <span class="title">Amelia Intan</span>
+      </div>
+      <span class="content">I am very impressed with the quality of service provided by this cat adoption website. I deeply appreciate the selective adoption process, as it gives me confidence that the cat I adopt is healthy and vaccinated. I truly enjoy my adoption experience through this website.</span>
+    </div>
+  </div>
+  <p>
+    <div style="position: relative; width: 100%; height: 100%;">
+      
+      <div class="container mx-auto">
+        <img src="/images/Admin.png" alt="Gambar" style="padding-left: 70px;padding-top: 20px;padding-right: 20px; position: flex; left: 10px; top: 0.5px; width: 81px; height: 81px;">
+        <div class="box mx-auto">
+          <div class="wrapper-header">
+          </div>
+          <span class="content">Thank you for your trust and support in choosing our website for cat adoption. If you have any further questions or require assistance, please don't hesitate to reach out to us.</span>
+        </div>
+      </div>
+    </div>
+  
+
+  <div class="container mx-auto">
+    <div class="box mx-auto">
+      <div class="wrapper-header">
+        <img src="/images/bintang2.png">
+        <span class="title">Permata Cinta Via</span>
+      </div>
+      <span class="content">I would like to share my positive experience with this cat adoption website. The search system provided made it easy for me to find a cat that matches my preferences. The adoption process was very transparent, and I felt secure in entrusting my home to the cat I adopted.</span>
+    </div>
+  </div>
+  <p>
+    <div style="position: relative; width: 100%; height: 100%;">
+      
+      <div class="container mx-auto">
+        <img src="/images/Admin.png" alt="Gambar" style="padding-left: 70px;padding-top: 20px;padding-right: 20px; position: flex; left: 10px; top: 0.5px; width: 81px; height: 81px;">
+        <div class="box mx-auto">
+          <div class="wrapper-header">
+          </div>
+          <span class="content">Thank you for sharing your positive experience with our cat adoption website. We are thrilled to hear that you had a great time using our services.</span>
+        </div>
+      </div>
+    </div>
+
+  <div class="container mx-auto">
+    <div class="box mx-auto">
+      <div class="wrapper-header">
+        <img src="/images/bintang.png">
+        <span class="title">Indah Kumalasari</span>
+      </div>
+      <span class="content">This cat adoption website is the best place to find the perfect pet cat. I have adopted two cats through this website, and both experiences have been amazing. I am highly impressed with the friendly customer service and assistance provided by the website team.</span>
+    </div>
+  </div>
+  <p>
+    <div style="position: relative; width: 100%; height: 100%;">
+      
+      <div class="container mx-auto">
+        <img src="/images/Admin.png" alt="Gambar" style="padding-left: 70px;padding-top: 20px;padding-right: 20px; position: flex; left: 10px; top: 0.5px; width: 81px; height: 81px;">
+        <div class="box mx-auto">
+          <div class="wrapper-header">
+          </div>
+          <span class="content">Thank you for your testimonial about our cat adoption website. We're thrilled that you have had amazing experiences adopting two cats through our website. It's always a joy for us to be a part of the journey in bringing new furry friends into loving homes.</span>
+        </div>
+      </div>
+    </div>
+
+  <div class="container mx-auto">
+    <div class="box mx-auto">
+      <div class="wrapper-header">
+        <img src="/images/bintang3.png">
+        <span class="title">Eka Abimanyu</span>
+      </div>
+      <span class="content">I recently adopted a cat through this cat adoption website, and I couldn't be happier. I received comprehensive information about the cat I wanted to adopt, and I was able to contact the previous owner to ask more questions. The cat I adopted is incredibly cute and sweet. Thank you so much!</span>
+    </div>
+  </div>
+  <p>
+    <div style="position: relative; width: 100%; height: 100%;">
+      
+      <div class="container mx-auto">
+        <img src="/images/Admin.png" alt="Gambar" style="padding-left: 70px;padding-top: 20px;padding-right: 20px; position: flex; left: 10px; top: 0.5px; width: 81px; height: 81px;">
+        <div class="box mx-auto">
+          <div class="wrapper-header">
+          </div>
+          <span class="content">Thank you for sharing your testimonial about adopting a cat through our cat adoption website. It's delightful to know that the cat you adopted is incredibly cute and sweet. We hope that your new furry friend brings you immense joy and companionship.</span>
+        </div>
+      </div>
+    </div>
 </body>
  
 </html>
