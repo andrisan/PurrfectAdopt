d838152af815a938493b80f9acf1c8b36a25462b
Taufik Hidayat <taufikhdyt@student.ub.ac.id>
2023-06-18 20:45:23 +0700

    menambahkan href ke detail

--- resources/views/article/article.blade.php
+++ resources/views/article/article.blade.php
@@ -95,7 +95,7 @@
                 {{ Str::limit($content->isi, 200) }}
             </p>
             <br>
-            <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-yellow-400 rounded-lg hover:bg-yellow-500">
+            <a href="/article/details" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-yellow-400 rounded-lg hover:bg-yellow-500">
                 Read More
                 <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
             </a>
