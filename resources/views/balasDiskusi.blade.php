b20c976938457b0cdbfedbb1caf42c3a69dce92d
abdulrasyidhimawan <himawanrasyid91@student.ub.ac.id>
2023-06-18 15:28:29 +0700

    Update balasDiskusi.blade.php

--- resources/views/balasDiskusi.blade.php
+++ resources/views/balasDiskusi.blade.php
@@ -24,20 +24,6 @@
           <h1 style="font-size: 20px; padding-left: 25px; padding-top: 25px; font-weight: bold">Ferry Pangateu</h1>
         </div>
         <p style="font-size: 1.2vw; padding-left: 55px; padding-bottom: 15px">Bagaimana latar belakang kesehatan kucing ini? Apakah ada riwayat penyakit atau kondisi kesehatan tertentu yang perlu diperhatikan?</p>
-        <div class="flex">
-          <div style="padding-left: 70px">
-            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 14 14">
-              <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" d="m7 12.45l-5.52-5c-3-3 1.41-8.76 5.52-4.1c4.11-4.66 8.5 1.12 5.52 4.1Z" />
-            </svg>
-          </div>
-          <h1 style="font-size: 1.2vw; padding-left: 25px; padding-top: 5">1500 suka</h1>
-          <div style="padding-left: 70px">
-            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24">
-              <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 4H5a2 2 0 0 0-2 2v15l3.467-2.6a2 2 0 0 1 1.2-.4H19a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2z" />
-            </svg>
-          </div>
-          <h1 style="font-size: 1.2vw; padding-left: 25px; padding-top: 5">350 komentar</h1>
-        </div>
       </div>
     </div>
 
@@ -75,50 +61,6 @@
       </div>
     </div>
     
-
-    <div class="flex justify-center items-center">
-      <svg style="padding-left: 15px; padding-top: 45px" xmlns="http://www.w3.org/2000/svg" width="115" height="115" viewBox="0 0 24 24">
-        <path
-          fill="currentColor"
-          d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10s10-4.48 10-10S17.52 2 12 2zm0 4c1.93 0 3.5 1.57 3.5 3.5S13.93 13 12 13s-3.5-1.57-3.5-3.5S10.07 6 12 6zm0 14c-2.03 0-4.43-.82-6.14-2.88a9.947 9.947 0 0 1 12.28 0C16.43 19.18 14.03 20 12 20z"
-        />
-      </svg>
-      <div class="kotak3">
-        <div style="padding-left: 25px; padding-top: 20px">
-          <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24">
-            <path
-              fill="currentColor"
-              d="M14.36 14.23a3.76 3.76 0 0 1-4.72 0a1 1 0 0 0-1.28 1.54a5.68 5.68 0 0 0 7.28 0a1 1 0 1 0-1.28-1.54ZM9 11a1 1 0 1 0-1-1a1 1 0 0 0 1 1Zm6-2a1 1 0 1 0 1 1a1 1 0 0 0-1-1Zm-3-7a10 10 0 1 0 10 10A10 10 0 0 0 12 2Zm0 18a8 8 0 1 1 8-8a8 8 0 0 1-8 8Z"
-            />
-          </svg>
-        </div>
-        <p style="font-size: 1.2vw; padding-left: 70px; padding-top: px; padding-right: 60px">Ketik pesan Anda disini</p>
-        <div style="padding-left: 825px">
-          <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24">
-            <path
-              fill="none"
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="1.5"
-              d="m21.438 11.662l-9.19 9.19a6.003 6.003 0 1 1-8.49-8.49l9.19-9.19a4.002 4.002 0 0 1 5.66 5.66l-9.2 9.19a2.001 2.001 0 1 1-2.83-2.83l8.49-8.48"
-            />
-          </svg>
-        </div>
-        <div style="padding-left: 890px">
-          <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24">
-            <g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5">
-              <path d="M2 19V9a2 2 0 0 1 2-2h.5a2 2 0 0 0 1.6-.8l2.22-2.96A.6.6 0 0 1 8.8 3h6.4a.6.6 0 0 1 .48.24L17.9 6.2a2 2 0 0 0 1.6.8h.5a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2Z" />
-              <path d="M12 17a4 4 0 1 0 0-8a4 4 0 0 0 0 8Z" />
-            </g>
-          </svg>
-        </div>
-      </div>
-      <div style="padding-top: 40px; padding-left: 25px; padding-right: 20px">
-        <svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" viewBox="0 0 24 24">
-          <path fill="currentColor" d="M12 2a10 10 0 0 1 10 10a10 10 0 0 1-10 10A10 10 0 0 1 2 12A10 10 0 0 1 12 2M8 7.71v3.34l7.14.95l-7.14.95v3.34L18 12L8 7.71Z" />
-        </svg>
-      </div>
     </div>
   </body>
 </html>
