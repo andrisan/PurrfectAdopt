038dfe5e0a085ab8510cf927d9218687424b7016
abdulrasyidhimawan <himawanrasyid91@student.ub.ac.id>
2023-06-18 15:34:40 +0700

    balas diskusi

--- resources/views/balasDiskusi.blade.php
+++ resources/views/balasDiskusi.blade.php
@@ -40,24 +40,6 @@
           dari penyakit menular dan telah divaksinasi lengkap sesuai dengan jadwal yang disarankan oleh dokter hewan. Namun, penting untuk diingat bahwa sebagai calon pemilik, Anda perlu melakukan pemeriksaan kesehatan rutin dan memberikan
           perawatan yang tepat untuk memastikan kucing tetap sehat dan bahagia.
         </p>
-        <div class="flex"></div>
-      </div>
-    </div>
-
-    <div class="flex justify-center items-center">
-      <svg style="padding-left: 15px; padding-top: 30px" xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 24 24">
-        <path
-          fill="currentColor"
-          d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10s10-4.48 10-10S17.52 2 12 2zm0 4c1.93 0 3.5 1.57 3.5 3.5S13.93 13 12 13s-3.5-1.57-3.5-3.5S10.07 6 12 6zm0 14c-2.03 0-4.43-.82-6.14-2.88a9.947 9.947 0 0 1 12.28 0C16.43 19.18 14.03 20 12 20z"
-        />
-      </svg>
-      <div class="kotak2">
-        <p style="font-size: 1.2vw; padding-left: 35px; padding-bottom: 15px; padding-top: 35px; padding-right: 60px">
-          Kucing ini telah menjalani pemeriksaan kesehatan secara menyeluruh oleh seorang dokter hewan terpercaya. Saat ini, kucing dalam kondisi sehat tanpa riwayat penyakit yang perlu diperhatikan. Kami memastikan bahwa kucing ini bebas
-          dari penyakit menular dan telah divaksinasi lengkap sesuai dengan jadwal yang disarankan oleh dokter hewan. Namun, penting untuk diingat bahwa sebagai calon pemilik, Anda perlu melakukan pemeriksaan kesehatan rutin dan memberikan
-          perawatan yang tepat untuk memastikan kucing tetap sehat dan bahagia.
-        </p>
-        <div class="flex"></div>
       </div>
     </div>
     
