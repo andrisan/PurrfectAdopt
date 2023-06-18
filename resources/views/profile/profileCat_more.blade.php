bdfc20a242fe81987115ea2bed788bb03f222300
anggoro1234 <anggoror179@gmail.com>
2023-06-18 21:24:36 +0700

    Mengumpulkan code view section profileCat_more

--- resources/views/profile/profileCat_more.blade.php
+++ resources/views/profile/profileCat_more.blade.php
@@ -21,11 +21,37 @@
 </head>
 <body>
     @include('components.header');
-    
-
-
-
-
+    <div class="container mx-auto flex flex-wrap md:flex-nowrap">
+    <div class="sticky">
+    <img src="{{asset('/img/Vector.png')}}" alt="" class="w-10">
+    </div>
+        <div class="w-full xl:w-4/8 py-7 mt-7">
+            <img class="w-3/4 h-auto mx-auto" src="{{ asset('/img/Kuceng.png') }}">
+        </div>
+        <div class="flex flex-col xl:w-3/4 mt-14 text-left">
+        <h1 class="text-6xl mb-8 text-black">BUDYANTO</h1>
+        <h3 class="">PET Description</h3>
+        <ul class="list-disc ml-6 mt-2">
+        <li>Type  : Cat</li>
+        <li>Sex   : Male</li>
+        <li>Breed : British Short Hair</li>
+        <li>Age   : 1 Years</li>
+        <li>Color : Brown</li>
+        <li>Weight: 10 Lbs</li>
+        </ul>
+        <div>
+        <p class="block capitalize text-2xl text-yellow-700 mt-4">details</p>
+        <div class="flex mt-1">
+        <div class="box border rounded p-1 w-1 h-21 bg-yellow-400">
+        </div>
+        <div class="box border rounded p-2 w-3/4 ml-3 bg-yellow-400">
+         <h5 class="">Kucing yang termasuk dalam spesies British Short Hair, yang memiliki ciri khas bulu pendek dan padat. Kucing ini memiliki warna bulu yang indah berupa nuansa coklat yang memikat mata.</h5>
+        </div>
+    </div>
+        </div>
+        </div>
+    </div>
+</div>
     @include('components.footer');
 </body>
 </html>
