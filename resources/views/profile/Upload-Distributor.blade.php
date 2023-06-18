452f70aa7e8e8dd924e0056118e9e82ca9789775
AdznlHakim12 <adzinulhakim04@student.ub.ac.id>
2023-06-18 22:30:04 +0700

    update page update distributor

--- resources/views/profile/Upload-Distributor.blade.php
+++ resources/views/profile/Upload-Distributor.blade.php
@@ -24,10 +24,10 @@
     <body>
         @include('components.header');
 
-    <div class="flex items-center justify-center h-screen rounded lg">
-    <div class="bg-yellow-300 w-2/3 p-4 rounded-lg">
+        <div class="flex items-center justify-center h-screen rounded-lg">
+    <div class="bg-yellow-300 w-4/5 p-4 rounded-lg">
         <div class="flex">
-            <div class="w-1/2 p-2 rounded-lg bg-gray-200 opacity-70">
+            <div class="w-1/3 p-4 rounded-lg bg-gray-200 opacity-70">
                 <div class="flex items-center justify-center h-full border-dashed border-2 border-gray-400 rounded-lg">
                     <div class="text-4xl text-gray-500 mb-2">
                     </div>
@@ -37,30 +37,30 @@
                 </div>
             </div>
 
-            <div class="w-1/2 p-2 ml-2">
-                <div class="bg-white h-16 mb-2 rounded-lg flex items-center pl-4">
+            <div class="w-2/3 p-4 ml-4">
+                <div class="bg-white h-20 mb-4 rounded-lg flex items-center pl-4">
                     <input
                         type="text"
-                        class="w-full h-full p-4 text-lg text-gray-500 outline-none rounded"
+                        class="w-full h-full p-4 text-lg text-gray-500 outline-none rounded-lg"
                         placeholder="Title">
                 </div>
-                <div class="bg-white h-16 mb-2 rounded-lg flex items-center pl-4">
+                <div class="bg-white h-20 mb-4 rounded-lg flex items-center pl-4">
                     <input
                         type="text"
-                        class="w-full h-full p-4 text-lg text-gray-500 outline-none rounded"
+                        class="w-full h-full p-4 text-lg text-gray-500 outline-none rounded-lg"
                         placeholder="Your Address">
                 </div>
-                <div class="bg-white h-32 mb-2 rounded-lg">
+                <div class="bg-white h-32 mb-4 rounded-lg">
                     <textarea
                         id="caption"
-                        class="w-full h-full p-4 text-lg text-gray-500 resize-none outline-none rounded"
+                        class="w-full h-full p-4 text-lg text-gray-500 resize-none outline-none rounded-lg"
                         placeholder="Caption"></textarea>
                 </div>
-                <div class="bg-white h-16 mb-2 rounded-lg flex items-center pl-4">
+                <div class="bg-white h-20 mb-4 rounded-lg flex items-center pl-4">
                     <input
                         id="phone-number"
                         type="text"
-                        class="w-full h-full p-4 text-lg text-gray-500 outline-none rounded"
+                        class="w-full h-full p-4 text-lg text-gray-500 outline-none rounded-lg"
                         placeholder="Phone Number">
                 </div>
                 <div class="flex items-end">
@@ -70,7 +70,7 @@
         </div>
         <div class="flex justify-end">
             <button
-                class="bg-green-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2">
+                class="bg-green-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg mt-2">
                 Save
             </button>
         </div>
