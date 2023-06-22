36d661338cb1716d375b91a9c0c38e747a9b1674
diwidiiass <diwiwardhana12@gmail.com>
2023-06-22 21:06:08 +0700

    Menambahkan revisi view section 2

--- resources/views/profile/profile_adopter.blade.php
+++ resources/views/profile/profile_adopter.blade.php
@@ -51,7 +51,7 @@
         </div>
     </div>
     <!-- p iki form -->
-    <div class="bg-white p-3 m-5 rounded-lg hover:shadow">
+    <div class="bg-yellow-300 p-3 m-5 rounded-lg hover:shadow">
     @php
         $editing = false; // Menambahkan inisialisasi variabel $editing
     @endphp
@@ -91,6 +91,49 @@
         </div>
     @endif
 </div>
+
+<!-- section 2 -->\
+
+<!-- Container for demo purpose -->
+<div class="container my-24 mx-auto md:px-6">
+  <!-- Section: Design Block -->
+  <section class="mb-32 text-center md:text-left">
+    <div
+      class="bg-yellow-300 block rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
+      <div class="bg-yellow-300 flex flex-wrap items-center">
+        <div class="block w-full shrink-0 grow-0 basis-auto lg:flex lg:w-6/12 xl:w-4/12">
+          <img src="{{ asset($user->fotoProfil) }}" alt="Trendy Pants and Shoes"
+            class="w-full rounded-t-lg lg:rounded-tr-none lg:rounded-bl-lg" />
+        </div>
+        <div class="w-full shrink-0 grow-0 basis-auto lg:w-6/12 xl:w-8/12">
+          <div class="bg-yellow-300 px-6 py-12 md:px-12">
+            <h2 class="mb-2 text-3xl font-bold text-primary dark:text-primary-400">{{ $user->name }}</h2>
+            <p class="mb-4 font-semibold">{{ $user->alamat }}</p>
+            <p class="mb-6 text-black dark:text-black text-justify">
+              Ad, at blanditiis quaerat laborum officia incidunt cupiditate
+              dignissimos voluptates eius aliquid minus praesentium!
+              Perferendis et totam ipsum ex aut earum libero accusamus
+              voluptas quod numquam saepe, consequuntur nihil quia tenetur
+              consequatur. Quis, explicabo deserunt quasi assumenda ea
+              maiores nulla, et dolor saepe praesentium natus error
+              reiciendis voluptas iste. Esse, laudantium ipsum animi, quos
+              voluptatibus atque vero repellat sit eligendi autem maiores
+              quasi cum aperiam. Aperiam rerum culpa accusantium, ducimus
+              deserunt aliquid alias vitae quasi corporis placeat vel
+              maiores explicabo commodi!
+            </p>
+            <div class="flex justify-end">
+                <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">More</button>
+                <button type="submit" class="mx-5 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">My Pet</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- Section: Design Block -->
+</div>
+<!-- Container for demo purpose -->
 
 <!-- custom js -->
 <script>
