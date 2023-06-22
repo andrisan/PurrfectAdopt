18b93926ec6058f5aae4cc3bc6030f021eb74e79
Muhammad Dhivara Aurian <betaindo45@gmail.com>
2023-06-22 08:58:59 +0700

    Update CatController.php

--- app/Http/Controllers/CatController.php
+++ app/Http/Controllers/CatController.php
@@ -8,17 +8,6 @@ use Illuminate\Http\Request;
 
 class CatController extends Controller
 {
-public function adopted()
-    {
-        $cats = [
-            ['name' => 'Kucing Dummy 1'],
-            ['name' => 'Kucing Dummy 2'],
-            // Tambahkan data kucing dummy lainnya sesuai kebutuhan
-        ];
-
-        return view('cats.adopted', compact('cats'));
-    }
-
     public function index(){
         $cats = Kucing::all();
         return view('/cats/adopted', compact('cats'));
