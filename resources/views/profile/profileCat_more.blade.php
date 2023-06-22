5a5f5a807b1cc8d02790579df8e7d69bc67361bc
anggoro1234 <anggoror179@gmail.com>
2023-06-22 20:11:43 +0700

    Mengubah detail kucing menjadi dinamis

--- resources/views/profile/profileCat_more.blade.php
+++ resources/views/profile/profileCat_more.blade.php
@@ -23,32 +23,31 @@
     @include('components.header');
     <div class="container mx-auto flex flex-wrap md:flex-nowrap mt-20">
     <div id="love-container">
-    <button id="toggleButton" onclick="toggleHeart()"><i id="heartIcon" class="fa-regular fa-heart fa-2xl" style="color: #ff0505;"></i></button>
-</div>
-        <div class="w-full xl:w-4/8 py-7 mt-7">
-            <img class="w-3/4 h-auto mx-auto" src="{{ asset('/img/Kuceng.png') }}">
-        </div>
-        <div class="flex flex-col xl:w-3/4 mt-20 text-left">
-        <h1 class="text-6xl mb-8 text-black">BUDYANTO</h1>
+        <button id="toggleButton" onclick="toggleHeart()"><i id="heartIcon" class="fa-regular fa-heart fa-2xl" style="color: #ff0505;"></i></button>
+    </div>
+    <div class="w-full xl:w-4/8 py-7 mt-7">
+        <img class="w-3/4 h-80 mx-auto" src="{{ asset($kucing->foto) }}">
+    </div>
+    <div class="flex flex-col xl:w-3/4 mt-20 text-left">
+        <h1 class="text-6xl mb-8 text-black">{{ $kucing->nama }}</h1>
         <h3 class="">PET Description</h3>
         <ul class="list-disc ml-6 mt-2">
-        <li>Type  : Cat</li>
-        <li>Sex   : Male</li>
-        <li>Breed : British Short Hair</li>
-        <li>Age   : 1 Years</li>
-        <li>Color : Brown</li>
-        <li>Weight: 10 Lbs</li>
+            <li>Type  : Cat</li>
+            <li>Sex   : {{ $kucing->gender ? 'Male' : 'Female' }}</li>
+            <li>Breed : {{ $kucing->ras }}</li>
+            <li>Age   : {{ $kucing->age }} Years</li>
+            <li>Color : {{ $kucing->warna }}</li>
+            <li>Weight: {{ $kucing->berat_badan }} Kg</li>
+            <li>Height: {{ $kucing->tinggi_badan}} Cm</li>
         </ul>
-        <div>
-        <p class="block capitalize text-2xl text-yellow-700 mt-4">details</p>
-        <div class="flex mt-1">
-        <div class="box border rounded p-1 w-1 h-21 bg-yellow-400">
-        </div>
-        <div class="box border rounded p-2 w-3/4 ml-3 bg-yellow-400">
-         <h5 class="">Kucing yang termasuk dalam spesies British Short Hair, yang memiliki ciri khas bulu pendek dan padat. Kucing ini memiliki warna bulu yang indah berupa nuansa coklat yang memikat mata.</h5>
-        </div>
-    </div>
-        </div>
+        <div class="mb-20">
+            <p class="block capitalize text-2xl text-yellow-700 mt-4">details</p>
+            <div class="flex mt-1">
+                <div class="box border rounded p-1 w-1 h-21 bg-yellow-400"></div>
+                <div class="box border rounded p-2 w-3/4 ml-3 bg-yellow-400">
+                    <h5 class="">{{ $kucing->description}} </h5>
+                </div>
+            </div>
         </div>
     </div>
 </div>
